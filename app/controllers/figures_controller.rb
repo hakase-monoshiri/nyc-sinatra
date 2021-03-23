3ac7aa2209378da2b4b2363cb58f0ef233f3db76
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-03-23 17:39:55 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,63 @@
 class FiguresController < ApplicationController
+
   # add controller methods
+  get "/figures" do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  get "/figures/new" do
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :'figures/new'
+  end
+
+  post "/figures" do
+   @figure = Figure.create(params[:figure])
+    
+    @landmark = Landmark.find_or_create_by(params[:landmark])
+    @landmark.figure = @figure
+    @landmark.save
+
+     @title = Title.find_or_create_by(params[:title])
+     @figure.titles << @title
+     @title.save
+   
+    @figure.save
+  
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  get "/figures/:id" do
+
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  get "/figures/:id/edit" do
+    @figure = Figure.find(params[:id])
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :'figures/edit'
+  end
+
+  patch "/figures/:id" do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+    
+    @landmark = Landmark.find_or_create_by(params[:landmark])
+    @landmark.figure = @figure
+    @landmark.save
+
+     @title = Title.find_or_create_by(params[:title])
+     @figure.titles << @title
+     @title.save
+   
+    @figure.save
+  
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  
 end
+  
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,55 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  get "/landmarks" do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+
+  get "/landmarks/new" do
+
+    erb :'landmarks/new'
+  end
+
+  post "/landmarks" do
+    # params[:landmark][:year_created] = params[:landmark][:year_created].to_i
+    @figure = Figure.find_by(name: params[:landmark][:figure])
+    if @figure
+      params[:landmark][:figure] = @figure
+    else
+      params[:landmark][:figure] = Figure.create(name: params[:landmark][:figure])
+    end
+    @landmark = Landmark.create(params[:landmark])
+  
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
+  get "/landmarks/:id" do
+
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/show'
+  end
+
+  get "/landmarks/:id/edit" do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/edit'
+  end
+
+  patch "/landmarks/:id" do
+    @landmark = Landmark.find(params[:id])
+    @figure = Figure.find_by(name: params[:landmark][:figure])
+    if @figure
+      params[:landmark][:figure] = @figure
+    else
+      params[:landmark][:figure] = Figure.create(name: params[:landmark][:figure])
+    end
+    @landmark.update(params[:landmark])
+    @landmark.save
+
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
+  delete "/landmarks/:id" do
+  end
+
+
 end
